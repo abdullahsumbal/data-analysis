d3c275752bb1d1286bf4d15ce06d8cd835ea836c
abdullahsumbal <abdullahsumbal@gmail.com>
2019-06-02 04:38:15 -0400

    more controls

---
+++ karlie/controllers/validate_file.py
@@ -0,0 +1,13 @@
+
+def validate_medusa_file(file_name):
+    pass
+
+def validate_mass_file(file_name):
+    pass
+def validate_config_file(file_name):
+    pass
+
+def validate_x_y_file(x_y_file):
+    pass
+
+
