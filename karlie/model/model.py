4c3468764a4b347720e346ce85a2fd7caacef0f0
abdullahsumbal <abdullahsumbal@gmail.com>
2019-06-03 01:09:47 -0400

    minor schanges

--- karlie/model/model.py
+++ karlie/model/model.py
@@ -1,6 +1,5 @@
 from PyQt5.QtCore import QObject, pyqtSignal
 
-
 class Medusa(QObject):
     file_name_changed = pyqtSignal(str, str)
 
@@ -12,9 +11,10 @@ class Medusa(QObject):
         self._x_y_file_name = ""
         self._config_file_name = ""
         self._file_name = ""
-        self._file_data = None
-        self._file_header = None
-        self._resistances = None
+        self._medusa_data = None
+        self._mass_data = None
+        self._x_y_data = None
+        self._config_data = None
 
 
     @property
@@ -24,7 +24,8 @@ class Medusa(QObject):
     @file_name.setter
     def file_name(self, value):
         name = value[0]
-        file_type = value[1]
+        data = value[1]
+        file_type = value[2]
 
         if file_type == "medusa":
             self._medusa_file_name = name
@@ -36,33 +37,3 @@ class Medusa(QObject):
             self._config_file_name = name
 
         self.file_name_changed.emit(name, file_type)
-
-
-    @property
-    def resistances(self):
-        return self._resistances
-
-    @property
-    def file_header(self):
-        return self._file_header
-
-    @property
-    def file_data(self):
-        return self._file_data
-
-
-
-    @file_header.setter
-    def file_header(self, value):
-        self._file_header = value
-        # self.file_name_changed.emit(value)
-
-    @file_data.setter
-    def file_data(self, value):
-        self._file_data = value
-        # self._file_data.emit(value)
-
-    @resistances.setter
-    def resistances(self, value):
-        self._resistances = value
-        # self._file_data.emit(value)
