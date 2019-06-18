ce6ba3505d2883e47411772d7b10e938889c48f6
abdullahsumbal <abdullahsumbal@gmail.com>
2019-06-18 14:23:49 -0400

    always yes

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
 git stash
 git pull origin master
 source activate data-analysis
-pyinstaller --noconsole -n karlie --distpath ~/Desktop karlie/app.py
+pyinstaller --noconsole -n karlie --distpath ~/Desktop -y karlie/app.py
