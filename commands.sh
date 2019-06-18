21dbfbdffa298b6ee721116749c3dc004d77a987
abdullahsumbal <abdullahsumbal@gmail.com>
2019-06-18 15:00:51 -0400

    refine instructions

--- commands.sh
+++ commands.sh
@@ -1,5 +1,4 @@
 rm -rf ~/data-analysis/
 git clone https://github.com/abdullahsumbal/data-analysis.git ~/data-analysis
-cd ~/data-analysis
 source activate data-analysis
-pyinstaller --noconsole -n karlie --distpath ~/Desktop -y karlie/app.py
+pyinstaller --noconsole -n karlie --distpath ~/Desktop -y ~/data-analysis/karlie/app.py
